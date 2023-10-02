c22cf3557b16f28d560a1e8e1b851ee3c02aec5f
ujjwal0 <83915211+ujjwal0@users.noreply.github.com>
2023-10-02 16:03:32 +0530

    2nd commit

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mailSender = require("../utils/mailSender");
+const mailSender = require("../utils/mailSender")
 const emailTemplate = require("../mail/templates/emailVerificationTemplate");
 const OTPSchema = new mongoose.Schema({
 	email: {
@@ -30,7 +30,7 @@ async function sendVerificationEmail(email, otp) {
 			"Verification Email",
 			emailTemplate(otp)
 		);
-		console.log("Email sent successfully: ", mailResponse.response);
+		console.log("Email sent successfully: ", mailResponse);
 	} catch (error) {
 		console.log("Error occurred while sending email: ", error);
 		throw error;
@@ -43,7 +43,7 @@ OTPSchema.pre("save", async function (next) {
 
 	// Only send an email when a new document is created
 	if (this.isNew) {
-		// await sendVerificationEmail(this.email, this.otp);
+		 await sendVerificationEmail(this.email, this.otp);
 	}
 	next();
 });
--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,19 +4,29 @@ const mailSender = async (email, title, body) => {
     try{
             let transporter = nodemailer.createTransport({
                 host:process.env.MAIL_HOST,
+                port:587,
                 auth:{
                     user: process.env.MAIL_USER,
                     pass: process.env.MAIL_PASS,
                 }
             })
-
-
-            let info = await transporter.sendMail({
-                from: 'StudyNotion || CodeHelp - by Babbar',
+            const mailOptions = {
+                from: 'ujjuwalia15@gmail.com',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
+            }
+
+            let info = await transporter.sendMail(mailOptions,(error,info)=>{
+                if(error){
+                    console.log(error);
+                }
+                else{
+                    console.log("Email Sent" , info.response);
+                }
             })
+                
+            
             console.log(info);
             return info;
     }
