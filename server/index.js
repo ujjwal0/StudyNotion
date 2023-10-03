8bed590900198e65faa65e6bb93dae69f42df315
ujjwal0 <83915211+ujjwal0@users.noreply.github.com>
2023-10-03 14:10:28 +0530

    hii

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-notion-ruby.vercel.app/", 
 		credentials:true,
 	})
 )
